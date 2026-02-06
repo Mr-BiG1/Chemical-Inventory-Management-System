95c89a95631dd940fbc66f74ffb47bd5be172d35
dsabugeorge9922 <darsansabu09@gmail.com>
2026-02-06 18:37:46 -0500

    created main menu and its options, aldo created font color cls

---
+++ UI/ColoredFont.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chemical_Inventory_Management_System.UI
+{
+    public class ColoredFont
+    {
+        public void ColorFont(ConsoleColor color,string context)
+        {
+            try 
+            {
+                var previous = Console.ForegroundColor;
+                Console.ForegroundColor = color;
+                Console.WriteLine(context);
+                Console.ForegroundColor = previous;
+            } catch (Exception e) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e); 
+            }
+        }
+    }
+}
--- UI/Menu.cs
+++ UI/Menu.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace Chemical_Inventory_Management_System.UI
 {
-    internal class Menu
+    public  class Menu
     {
+        public void MenuOptions() { 
+        {
+            ColoredFont styledFont = new ColoredFont();
+            // setting up the color 
+            Console.ForegroundColor = ConsoleColor.Green;
+            styledFont.ColorFont(ConsoleColor.Green, "Chemical inventory Management System");
+            
+            styledFont.ColorFont(ConsoleColor.Blue, "1. Search Chemical");
+            Console.WriteLine();
+            styledFont.ColorFont(ConsoleColor.Blue, "2. Add New Chemical");
+            Console.WriteLine();
+            styledFont.ColorFont(ConsoleColor.Blue, "3. Restock Chemical");
+            Console.WriteLine();
+            styledFont.ColorFont(ConsoleColor.Blue, "4. Edit Chemical");
+            Console.WriteLine();
+            styledFont.ColorFont(ConsoleColor.Blue, "5. View Expiry Alerts");
+            Console.WriteLine();
+            styledFont.ColorFont(ConsoleColor.Blue, "6. View Low Stock");
+            Console.WriteLine();
+            styledFont.ColorFont(ConsoleColor.Blue, "7. Exit");
+        }
     }
 }
